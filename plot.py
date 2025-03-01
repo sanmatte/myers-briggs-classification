7805c2516a37528609e95d0e4919a05cf2e2e6df
LukeMedTkd <lcuamedda191@gmail.com>
2025-03-01 20:11:11 +0100

    Add distribution questions chart

--- plot.py
+++ plot.py
@@ -1,6 +1,8 @@
 import matplotlib.pyplot as mpl
 import numpy as np
 import seaborn as sns
+from plotly.offline import iplot
+import plotly.express as px
 
 # Mostra la matrice di correlazione fra le diverse domande
 def correlation_matrix_chart(X, corr_method):
@@ -15,6 +17,26 @@ def correlation_matrix_chart(X, corr_method):
     # Mostra il grafico
     mpl.title(f"Matrice di Correlazione - Coefficiente di correlazione: {corr_method}", fontsize = 18)
     mpl.show()
+
+def question_distribution_chart(X):
+    hex_colors = [
+    "#FF0000", "#00FF00", "#0000FF", "#FFFF00", "#FF00FF", "#00FFFF", "#FFA500", "#800080",
+    "#008000", "#000080", "#800000", "#808000", "#008080", "#C0C0C0", "#808080", "#FFC0CB",
+    "#800000", "#A52A2A", "#800080", "#008000", "#000080", "#708090", "#2F4F4F", "#D2691E",
+    "#B22222", "#228B22", "#20B2AA", "#4682B4", "#F0E68C", "#FF4500", "#FFD700", "#ADFF2F",
+    "#8A2BE2", "#CD5C5C", "#FF6347", "#40E0D0", "#7FFF00", "#DAA520", "#B0C4DE", "#87CEEB",
+    "#778899", "#6A5ACD", "#FF69B4", "#20B2AA", "#F5DEB3", "#F0FFFF", "#32CD32", "#FFB6C1",
+    "#9932CC", "#FF8C00", "#87CEFA", "#E9967A", "#00CED1", "#DDA0DD", "#8B008B", "#556B2F",
+    "#BDB76B", "#ADFF2F", "#4B0082", "#800000", "#CD853F"
+    ]
+
+    qq = ['Q'+str(i) for i in range(1,len(X.columns)+1)]
+    X.columns = qq
+
+    e = 0
+    for i in qq:
+        iplot(px.histogram(X,x=i,color_discrete_sequence=[hex_colors[e]],text_auto=True))
+        e = e + 1
 
 # Mostra la distribuzione del dataset - Evidenzia la distribuzione ≈ Uniforme
 def distribution_chart(df):
